3f981658fa5f25917ed21051280c8386eae4f7da
DeEpRaJ <99066501+Deepraj0502@users.noreply.github.com>
2023-05-24 14:43:53 +0530

    update

---
+++ src/Components/LatestEventDetails.js
@@ -0,0 +1,22 @@
+export const events = [
+    {
+        img: [
+            "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F444862499%2F1394200320313%2F1%2Foriginal.20230213-054456?w=512&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C1200%2C600&s=5f077de2efa38fca69811aecdec575cd",
+            "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F444862499%2F1394200320313%2F1%2Foriginal.20230213-054456?w=512&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C1200%2C600&s=5f077de2efa38fca69811aecdec575cd"
+        ],
+        name: ["Money Expo India 2023","Money Expo India 2023"],
+        time: ["10:00 AM","10:00 AM"],
+        loc : ["Jio World Convention Centre","Jio World Convention Centre"],
+        date : ["Sat, Aug 12","Sat, Aug 12"]
+    },
+    {
+        img: [
+            "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F444862499%2F1394200320313%2F1%2Foriginal.20230213-054456?w=512&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C1200%2C600&s=5f077de2efa38fca69811aecdec575cd",
+            "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F444862499%2F1394200320313%2F1%2Foriginal.20230213-054456?w=512&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C1200%2C600&s=5f077de2efa38fca69811aecdec575cd"
+        ],
+        name: ["Money Expo India 2023","Money Expo India 2023"],
+        time: ["10:00 AM","10:00 AM"],
+        loc : ["Jio World Convention Centre","Jio World Convention Centre"],
+        date : ["Sat, Aug 12","Sat, Aug 12"]
+    }
+]
