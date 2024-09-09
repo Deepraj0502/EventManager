1dff856eb57e993e8c901a2d9a1c762f9c98f164
prathameshpanse51 <93512551+prathameshpanse51@users.noreply.github.com>
2024-09-09 17:08:29 +0530

    validation in mobile number

--- src/Components/Profile/MobileNumber.js
+++ src/Components/Profile/MobileNumber.js
@@ -16,6 +16,8 @@ export default function MobileNumber(props) {
   const db = getFirestore(app);
   const location = useLocation();
   const [mobno, setMobNo] = useState("");
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
 
   const getMobileNumber = async () => {
     const querySnapshot = await getDocs(collection(db, "users"));
@@ -37,6 +39,7 @@ export default function MobileNumber(props) {
     var mobileno = document.getElementById("mob-input").value;
     var regEx = /^([0-9]{10})+$/;
     if (mobileno.length !== 10 || !regEx.test(mobileno)) {
+      setError("Invalid Mobile Number");
       document.getElementById("mob-invalid").style.display = "block";
     } else {
       const querySnapshot = await getDocs(collection(db, "users"));
@@ -49,6 +52,11 @@ export default function MobileNumber(props) {
         }
       });
       setShow(false);
+      setSuccess("Number Updated successfully!");
+      document.getElementById("toast-simple4").style.display = "flex";
+      setTimeout(() => {
+        document.getElementById("toast-simple4").style.display = "none";
+      }, 3000);
       setMobNo(mobileno);
     }
   };
@@ -56,6 +64,21 @@ export default function MobileNumber(props) {
 
   return (
     <>
+      <div
+        id="toast-simple4"
+        class="hidden fixed top-5 right-5 border-2 border-[#52b788] z-[999] flex items-center w-full max-w-xs p-4 space-x-4 rtl:space-x-reverse text-gray-500 bg-white divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow dark:text-gray-400 dark:divide-gray-700 dark:bg-gray-800"
+        role="success"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          fill="#52b788"
+          className="w-6 h-6"
+        >
+          <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
+        </svg>
+        <div class="ps-4 text-sm font-semibold text-[#52b788]">{success}</div>
+      </div>
       {mobno === null && (
         <Button
           variant="primary"
@@ -93,7 +116,7 @@ export default function MobileNumber(props) {
               id="mob-invalid"
               style={{ color: "red", display: "none", marginTop: "-10px" }}
             >
-              Invalid Mobile Number
+              {error}
             </Form.Label>
           </Form>
         </Modal.Body>
